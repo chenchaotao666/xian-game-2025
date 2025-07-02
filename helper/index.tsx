5cb57834911b2a86c1b9ea278a2ab832a9fe7d5f
chenchaotao666 <chenchaotao@huawei.com>
2025-07-02 21:13:13 +0800

    删除全部代码

--- helper/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import LogReplayer from './replayer';
-
-const element = document.getElementById('root');
-createRoot(element).render(<LogReplayer />);
