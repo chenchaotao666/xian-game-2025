b45044674ff48d3cafe46260f96d0be3b4cc1dc8
Hoikan <hoikan.chen@gmail.com>
2025-07-05 15:26:18 +0800

    fix: actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,12 +1,5 @@
 // 行为树动作类导出
 export { AnalyzeAndSetStrategy } from './AnalyzeAndSetStrategy';
-export { ExecuteEscape } from './ExecuteEscape';
-export { ExecuteCapture } from './ExecuteCapture';
-export { ExecutePreparation } from './ExecutePreparation';
-export { ExecuteAttack } from './ExecuteAttack';
 
 // 游戏状态相关动作
-export { ExecutePickGenerals as executePickGeneral, ExecuteChooseBuff as executeChooseBuff, ExecuteTroopProduction, ExecuteFormationChange, ExecuteCaptureFlag } from './GameStateActions';
-
-// 战略动作
-export { executeProduceTroops, executeFormationChange, executeAttackFortress, executeIdle, executeDevelopment } from './StrategicActions';
+export * from './GameStateActions';
