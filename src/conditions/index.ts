8b49601f12571616c57b31dcf0ac89d7737042fa
Hoikan <hoikan.chen@gmail.com>
2025-07-05 14:19:45 +0800

    fix: condtion重复定义

--- src/conditions/index.ts
+++ src/conditions/index.ts
@@ -1,4 +1,3 @@
-export * from './GameConditions';
 export { CanChooseBuff } from './CanChooseBuff';
 export { NeedMoreTroops } from './NeedMoreTroops';
 export { ShouldChangeFormation } from './ShouldChangeFormation';
